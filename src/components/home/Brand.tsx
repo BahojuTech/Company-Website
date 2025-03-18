b5dc692b1ffac92b561eb88da9820301e4d2ab84
Chimev <chimevictor.CV@gmail.com>
2025-03-18 14:43:32 +0100

    Made Changes

--- src/components/home/Brand.tsx
+++ src/components/home/Brand.tsx
@@ -19,12 +19,6 @@ const Brand = ({title, img, children}: BrandProps) => {
             </div>
             <div className=' grid gap-5 flex-1 text-lg'>
              {children}
-                {/* <p>
-                At <span className='text-primary font-bold'>BAHOJU</span> , we believe that every brand has untapped potential waiting to be unlocked. Our mission is to recharge your brand, enhancing its value and positioning it for long-term success in the competitive digital landscape. We use innovative digital marketing strategies to breathe new life into your brand and help it thrive in an ever-changing market.
-                </p>
-                <p>
-                Your brand’s identity is the foundation of its success. We work with you to refine and elevate your brand’s image, ensuring it resonates with your target audience . Whether it’s a complete brand overhaul or a subtle refresh, we tailor our approach to fit your vision and goals.
-                </p> */}
             </div>
         </div>
        
