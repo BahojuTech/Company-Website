ed3a0f894f6b41bc6071b30a754014a23b96e507
Chimev <chimevictor.CV@gmail.com>
2025-03-19 20:25:03 +0100

    added revalidation for data

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,7 +5,7 @@ import { client } from '@/lib/sanity';
 import { blogType } from '@/lib/blogType';
 import BlogCard from '@/components/blog/BlogCard';
 
-export const revalidate = 30;
+export const revalidate = 60;
 
 export const metadata: Metadata = {
   title: "Blog"
